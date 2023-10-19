7af46b823bdd27cd215fdc106dcf32bede27d317
Gagik Voskerchan <gagikvoskerchyan@gmail.com>
2023-10-19 13:35:23 -0400

    The Firts Sprint Of My Project

---
+++ FinalProjectC#/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalProjectC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
